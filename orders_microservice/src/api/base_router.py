6302b6336465e4023a75dc53f618fc63d9b67a7a
MarsiFrilex <tokalexandra17@gmail.com>
2025-11-05 15:13:44 +0300

    Release repositories

--- orders_microservice/src/api/base_router.py
+++ orders_microservice/src/api/base_router.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
 
+from src.api.routes.orders_router import router as orders_router
+from src.api.routes.orders_items_router import router as orders_items_router
+from src.api.routes.items_router import router as items_router
+
 base_router = APIRouter(prefix="/api/v1")
+base_router.include_router(orders_router)
+base_router.include_router(orders_items_router)
+base_router.include_router(items_router)
